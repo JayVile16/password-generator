c9222547551244ee67a354fec80778310650ebde
J0hn@th@N <jnaviles616@gmail.com>
2021-12-01 16:13:48 -0500

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,8 +3,6 @@ import PassWanime
 import PassWfantasy
 
 genre = str(input('What genre do you want your password to be? Please enter a number (1. Anime, 2. Comics, 3. Fantasy & Sci-Fi): '))
-length = int(input('How long would you like the terms to be? Please enter a number: '))
-special = input('Do you want special characters? (y/n)')
 
 if genre == '1':
      PassWanime.anipassword()
