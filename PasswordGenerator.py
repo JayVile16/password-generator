62afa2b9cf6f7be79e29e2a594b40f581fa67d08
J0hn@th@N <jnaviles616@gmail.com>
2021-12-01 16:04:22 -0500

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,18 +3,17 @@ import PassWanime
 import PassWfantasy
 
 genre = str(input('What genre do you want your password to be? Please enter a number (1. Anime, 2. Comics, 3. Fantasy & Sci-Fi): '))
-length = int(input('How long would you like the password to be? Please enter a number: '))
-#special = input('Do you want special characters? (y/n)')
-#if special == 'n':
-for length:
-     if genre == '1':
-          PassWanime.anipassword()
+length = int(input('How long would you like the terms to be? Please enter a number: '))
+special = input('Do you want special characters? (y/n)')
+
+if genre == '1':
+     PassWanime.anipassword()
           
-     elif genre == '2':
-          PassWsuperheroes.SHpassword()
+elif genre == '2':
+     PassWsuperheroes.SHpassword()
           
-     elif genre == '3':
-          PassWfantasy.fantpassword()
+elif genre == '3':
+     PassWfantasy.fantpassword()
                
-     else:
-          print('Please enter one of the following numbers 1, 2, 3?')
+else:
+     print('Please enter one of the following numbers 1, 2, 3?')
