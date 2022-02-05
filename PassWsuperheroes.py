cef409e7fb760ba4131d4bd38e470b6a4a36a400
J0hn@th@N <jnaviles616@gmail.com>
2022-02-04 20:43:33 -0500

    Update PassWsuperheroes.py

--- PassWsuperheroes.py
+++ PassWsuperheroes.py
@@ -1,36 +1,83 @@
-import random
+mport random
+import sqlite3
 
-def SHpassword():
-    # Customize the password
-    length = int(input('How long would you like the terms to be? Please enter a number: '))
-    special = input('Do you want special characters? (y/n): ')
+ask = str(input("What genre of password do you want? (1. comics, 2. manga/anime, or 3. fantasy): "))
+spec = input("Do you want special characters? (Ex. '!' or '?') y/n: ")
 
-    characters = ['Batman', 'Superman', 'supes', 'manof', 'steel', 'Wonder','Woman' 'princess','Diana', 'Prince', 'pr!ncess', 'pr!nc3ss', 'Green','lantern', 'arrow', 'Flash', 'fastest', 'manalive', 'speedforce', 'wally','barry', 'lightning', 'dark','knight', 'greenL', 'wwoman', 'darkseid', 'DarkSeid', 'DS', 'nightwing', 'NightWing', 'robin', 'RedR', 'R3dR0bin', 'BoyWonder', 'boy', 'damien', 'superboy', 'Jonathan','Kent', 'Connor','Kent', 'Lex', 'Luthor', 'l3x', '1uth0r', 'joker', 'ClownPrince','crime', 'cl0wnpr!nce','cr1m3', 'BlueB', 'bluebeetle', 'blueBeetle', 'aquaman', 'jason', 'todd', 'red', 'hood', 'orphan', '0rphAn', 'redhood', 'Oracle', 'Batgirl', 'Spiderman', 'Spidey', 'Webhead', 'Thor', 'Captain', 'America', 'Ras','AlGhul', 'Ebenezer','Darrk', 'TheWrath', 'Azarael', 'Riddler', 'Bane', 'r?ddlEr', 'Cyborg', 'Martian', 'Manhunter', 'Jonn', 'Jonzz', 'Hawkgirl', 'Hawkman', 'Static', 'Shock', 'Icon', 'Ic0n', 'iC0n', 'Booster', 'Gold', 'Black', 'Jefferson', 'Bruce', 'Wayne', 'KalEl', 'kalel', 'west', ]
+digits = str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))
+special = ['!', '$', '*', '?', '@']
 
-    settings = ['Gotham', 'Metropolis', 'Themyscira', 'goth@m', 'm3trop0lis', 'Apokolips', 'aPokolips', '@pok0l!pS', 'Atlantis', 'Bludhaven', 'starCity', 'StarCity', 'NewGenesis', 'BatCave', 'Crime', 'Alley', 'LexCorp', 'Iceberg','Lounge', 'Sanctum','Sanctorum', 'Baxter', 'Building', 'Avengers', 'Tower', 'Wayne','Manor', 'Fortress', 'Solitude']
 
-    a = random.randint(0,9)
-    b = random.randint(0,9)
-    c = random.randint(0,9)
-    d = random.randint(0,9)
+conn = sqlite3.connect('pop_database.db')
+cursor = conn.cursor()
 
-    if length % 2 == 1:
-        charRandom = random.choice(characters)[:(length//2)+1]
-        charList = [charRandom.lower(), charRandom.title(), charRandom]
+
+def comicPS():
+    db = cursor.execute("SELECT Comics from Password_Database")
+    row = cursor.fetchall()
+
+    x = random.randint(0,len(row)-1)
+    y = random.randint(0,len(row)-1)
+
+    tup = row[x]+row[y]
+    word = ''.join(tup)
+
+    if spec == 'y':
+        options = [(word+random.choice(special)),(random.choice(special)+word+random.choice(special))]
+        password = random.choice(options) + digits
+        print(password)
+    elif spec == 'n':
+        password = word + digits
+        print(password)
     else:
-        charRandom = random.choice(characters)[:length//2]
-        charList = [charRandom.lower(), charRandom.title(), charRandom]
-          
-    setRandom = random.choice(settings)[:length//2]
-    setList = [setRandom.lower(), setRandom.title(), setRandom]
-
-    extra = ['!', '?', '$', '#', '%', '&', '*']
-
-    print('Generating password:')
-    if special == 'y':
-        password = random.choice(extra) + random.choice(charList) + random.choice(setList) + str(a) + str(b) + str(c) + str(d) + random.choice(extra)
-    elif special == 'n':
-        password = random.choice(charList) + random.choice(setList) + str(a) + str(b) + str(c) + str(d)
-    print('Password:', password)
+        print("Please enter y(yes) or n(no)")
 
 
+def mangaPS():
+    db = cursor.execute("SELECT Manga from Password_Database")
+    row = cursor.fetchall()
+
+    x = random.randint(0,len(row)-1)
+    y = random.randint(0,len(row)-1)
+
+    tup = row[x]+row[y]
+    word = ''.join(tup)
+
+    if spec == 'y':
+        options = [(word+random.choice(special)),(random.choice(special)+word+random.choice(special))]
+        password = random.choice(options) + digits
+        print(password)
+    elif spec == 'n':
+        password = word + digits
+        print(password)
+    else:
+        print("Please enter y(yes) or n(no)")
+
+def fantasyPS():
+    db = cursor.execute("SELECT Fantasy from Password_Database")
+    row = cursor.fetchall()
+
+    x = random.randint(0,len(row)-1)
+    y = random.randint(0,len(row)-1)
+
+    tup = row[x]+row[y]
+    word = ''.join(tup)
+
+    if spec == 'y':
+        options = [(word+random.choice(special)),(random.choice(special)+word+random.choice(special))]
+        password = random.choice(options) + digits
+        print(password)
+    elif spec == 'n':
+        password = word + digits
+        print(password)
+    else:
+        print("Please enter y(yes) or n(no)")
+
+if ask == '1':
+    comicPS()
+elif ask == '2':
+    mangaPS()
+elif ask == '3':
+    fantasyPS()
+else:
+    print('Please enter a valid number.')
